588cbd946a412651623129d91aa8cb5f13b1d4d6
Dorian Guillaume <drnguillaume@gmail.com>
2020-01-20 10:30:20 +0100

    Add Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VoyageListComponent } from './voyage-list/voyage-list.component';
+import { VoyageComponent } from './voyage/voyage.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'voyages',
+    component: VoyageListComponent
+  },
+  {
+    path: 'voyage/:id',
+    component: VoyageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'voyages'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/shared/voyage.service.ts
+++ src/app/shared/voyage.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VoyageService {
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private voyageService: VoyageService ) {}
+    
 }
