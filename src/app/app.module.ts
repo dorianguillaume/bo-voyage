9fbdcb7e1b52b0df87886869df1e5fb19f26de5d
Dorian Guillaume <drnguillaume@gmail.com>
2020-01-20 09:14:09 +0100

    add-files

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { InMemoryDataService } from './shared/in-memory-data.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
