02f58279255f330e50ded8b88f4d05f92d7a34db
Marc <mnoris.xeno@gmail.com>
2020-01-20 16:45:50 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/app/auth/auth-service.guard.ts
+++ src/app/auth/auth-service.guard.ts
@@ -1,6 +1,8 @@
 import {Injectable} from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, RoutesRecognized} from '@angular/router';
 import {AuthService} from '../shared/auth.service';
+import {pairwise} from 'rxjs/internal/operators/pairwise';
+import {filter} from 'rxjs/internal/operators/filter';
 
 @Injectable({
   providedIn: 'root'
@@ -8,15 +10,21 @@ import {AuthService} from '../shared/auth.service';
 
 export class AuthServiceGuard implements CanActivate {
 
-  constructor(private authService: AuthService, private router: Router) {
+  previousUrl;
+
+  constructor(private authservice: AuthService, private router: Router) {
 
   }
 
   canActivate() {
     let isAuth = this.authService.isAuthenticated()
 
-    if(!isAuth){
-      this.router.navigate(['/login'])
+    const isAuth = this.authservice.isAuthenticated();
+
+    if (!isAuth) {
+      /* Récupère l'URL de la page avant redirection */
+      this.previousUrl = this.router.getCurrentNavigation().initialUrl.toString();
+      this.router.navigate(['/login']);
     }
     return isAuth
   }
